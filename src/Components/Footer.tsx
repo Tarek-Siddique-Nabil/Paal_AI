b9e3cc6d3b75dbe3c5492ae3f0021bad0b54c3dc
Tarek Siddique Nabil <nabilh420@gmail.com>
2024-01-11 12:56:32 +0600

    add link in footer icon

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,46 +1,21 @@
 import Image from "next/image";
 import { Logo } from "./ui/logo";
+import { footerIcon } from "@/constant/footerIcon";
 
 export const Footer = () => {
   return (
     <footer className="flex flex-col md:flex-row justify-center items-center gap-5 py-10 my-10 border-t-2 border-base-vilolet">
       <Logo />
       <div className="flex justify-center gap-1">
-        <img
-          src="/twitter.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/telegram.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/discord.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/dextools.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/Coingecko.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/image 5.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
-        <img
-          src="/image 4.png"
-          alt="twitter"
-          className="md:w-14 md:h-14 h-10 w-10"
-        />
+        {footerIcon.map((icon, index) => (
+          <a key={index} href={icon.link} target="_blank">
+            <img
+              src={icon.image}
+              alt="twitter"
+              className="md:w-14 md:h-14 h-10 w-10"
+            />{" "}
+          </a>
+        ))}
       </div>
     </footer>
   );
---
+++ src/constant/footerIcon.ts
@@ -0,0 +1,22 @@
+export const footerIcon = [
+  { image: "/twitter.png", link: "https://twitter.com/PaalMind" },
+  { image: "/telegram.png", link: "https://t.me/paal_ai" },
+  { image: "/discord.png", link: "https://discord.gg/paalai" },
+  {
+    image: "/dextools.png",
+    link: "https://www.dextools.io/app/en/ether/pair-explorer/0x2a6c340bcbb0a79d3deecd3bc5cbc2605ea9259f",
+  },
+
+  {
+    image: "/Coingecko.png",
+    link: "https://www.coingecko.com/",
+  },
+  {
+    image: "/image 5.png",
+    link: "",
+  },
+  {
+    image: "/image 4.png",
+    link: "",
+  },
+];
